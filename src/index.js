cb6bf73daec6ae1cb0ebe08a45f61d8f9fb0b7ed
江冠德 <mr.sunboss@gmail.com>
2016-07-11 16:54:49 +0800

    修改 css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
  * @copyright 2015, Andrey Popp <8mayday@gmail.com>
  */
 
-import 'font-awesome/css/font-awesome.css';
+// import 'font-awesome/css/font-awesome.css';
+
+if (process.env.BROWSER) {
+  require('font-awesome/css/font-awesome.css');
+}
 
 import Icon from './Icon';
 import IconStack from './IconStack';
